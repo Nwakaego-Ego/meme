8c0f1748faa23380045167823c6c47571eadfa9a
Ego Nwaekpe <glorynwaekpe@gmail.com>
2024-02-03 20:50:58 +0100

    i added images for meme and also filter meme

--- src/app/components/meme/page.jsx
+++ src/app/components/meme/page.jsx
@@ -1,25 +1,81 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import "./meme.css";
 
 const memes = () => {
+  const [searchMeme, setSearchMeme] = useState("");
+
+  const memesData = [
+    { image: "/catchup.png", category: "football" },
+    { image: "/emoji.webp", category: "score" },
+    { image: "/football.jpg", category: "cooking" },
+    { image: "/guy.png", category: "win" },
+    { image: "/catchup.png", category: "clean_sheet" },
+    { image: "/emoji.webp", category: "goal" },
+    { image: "/football.jpg", category: "offside" },
+    { image: "/guy.png", category: "missed" },
+    { image: "/catchup.png", category: "kill" },
+    { image: "/emoji.webp", category: "toast" },
+    { image: "/football.jpg", category: "tears" },
+    { image: "/guy.png", category: "back_your_bag" },
+    { image: "/catchup.png", category: " penalty" },
+    { image: "/emoji.webp", category: "shot" },
+    { image: "/football.jpg", category: "plaster" },
+    { image: "/guy.png", category: "no_gree_gor_anybody" },
+    { image: "/catchup.png", category: "VAR" },
+    { image: "/emoji.webp", category: "cup" },
+    { image: "/football.jpg", category: "banter" },
+    { image: "/guy.png", category: "cook" },
+  ];
+
+  const filteredMeme = memesData.filter((meme) => {
+    return meme.category.toLowerCase().includes(searchMeme.toLowerCase());
+  });
+
+  console.log(filteredMeme);
+
   return (
     <div className="bg-blue-500 min-h-screen text-white p-4">
       <div className="max-w-4xl mx-auto">
         <div className="mb-10 mt-10 flex justify-center">
           <input
             type="text"
-            placeholder="Search..."
+            placeholder="search meme..."
             className="bg-blue-700 text-white px-4 py-2 rounded-l focus:outline-none"
+            onChange={(e) => setSearchMeme(e.target.value)}
           />
-          <button className="bg-blue-700 px-4 py-2 rounded-r">Search</button>
+          {/* <button className="bg-blue-700 px-4 py-2 rounded-r">Search</button> */}
         </div>
 
         <div className="grid-container">
-          {Array.from({ length: 20 }, (_, index) => (
-            <div key={index} className="grid-item">
-              {index + 1}
-            </div>
-          ))}
+          {/* {Array.from({ length: 20 }, (_, index) => (       ))}
+          <div key={index} className="grid-item">
+            {memes[index] && (
+              <>
+                <img src={memes[index].image} alt={`Meme ${index + 1}`} />
+                {memes[index].category}
+              </>
+            )}
+          </div> */}
+          {/* {filteredMeme.map((meme, index) => {
+            return (
+              <div key={index} className="grid-item">
+                <img src={meme.image} alt={`meme ${index + 1}`} />
+                {meme.category}
+              </div>
+            );
+          })} */}
+
+          {filteredMeme.length > 0 ? (
+            filteredMeme.map((meme, index) => (
+              <div key={index} className="grid-item">
+                <img src={meme.image} alt={`meme ${index + 1}`} />
+                {meme.category}
+              </div>
+            ))
+          ) : (
+            <div className="error-message">Input correct meme</div>
+          )}
         </div>
       </div>
     </div>
