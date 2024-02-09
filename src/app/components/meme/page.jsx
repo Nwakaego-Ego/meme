e9ee7eefa5735736b60ea9ff8031204b9fe926ab
Ego Nwaekpe <glorynwaekpe@gmail.com>
2024-02-09 19:35:52 +0100

    styles the upload and added  finishing touches

--- src/app/components/meme/page.jsx
+++ src/app/components/meme/page.jsx
@@ -1,84 +1,3 @@
-// "use client";
-// import React, { useState, useEffect } from "react";
-// import Link from "next/link";
-// import { storage } from "../firebase/firebase";
-// import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
-// import "./meme.css";
-
-// const memes = () => {
-//   const [searchMeme, setSearchMeme] = useState("");
-//   const [imageData, setImageData] = useState([]);
-
-//   console.log(imageData);
-
-//   const imageListRef = ref(storage, "images/");
-
-//   const filteredMeme = imageData.filter((meme) => {
-//     return meme.name.toLowerCase().includes(searchMeme.toLowerCase());
-//   });
-
-//   const getImageName = (fullName) => {
-//     return fullName.split(".")[0];
-//   };
-
-//   useEffect(() => {
-//     listAll(imageListRef).then((response) => {
-//       console.log(response?.items);
-//       response.items.forEach((item) => {
-//         getDownloadURL(item).then((url) => {
-//           setImageData((prev) => [
-//             ...prev,
-//             { name: getImageName(item.name), url },
-//           ]);
-//         });
-//       });
-//     });
-//   }, []);
-
-//   return (
-//     <div className="bg-blue-500 min-h-screen text-white p-4">
-//       <div className="max-w-4xl mx-auto">
-//         <div className="mb-10 mt-10 flex justify-center">
-//           <input
-//             type="text"
-//             placeholder="search meme..."
-//             className="bg-blue-700 text-white px-4 py-2 rounded-l focus:outline-none"
-//             onChange={(e) => setSearchMeme(e.target.value)}
-//           />
-//           {/* <button className="bg-blue-700 px-4 py-2 rounded-r">Search</button> */}
-//           <Link href="/">
-//             <button
-//               // onClick={uploadImage}
-//               className="bg-blue-700 px-4 py-2 rounded-r ml-[50px]"
-//             >
-//               Home
-//             </button>
-//           </Link>
-//         </div>
-
-//         <div className="grid-container">
-//           {filteredMeme.length > 0 ? (
-//             filteredMeme.map((meme, index) => (
-//               <div key={index} className="grid-item">
-//                 <img src={meme.url} alt={meme.name} />
-//                 {meme.name}
-//               </div>
-//             ))
-//           ) : (
-//             <div className="error-message flex flex-col items-center justify-center text-center text-4xl font-bold text-red-500 p-8 bg-gray-200 rounded-md shadow-md ml-[350px]">
-//               <span className="mb-4">Loading</span>
-//               <div className="w-16 h-1 bg-red-500 rounded-full"></div>
-//               <span className="mt-4 text-sm text-gray-600">In a minute</span>
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default memes;
-
 "use client";
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
@@ -148,7 +67,7 @@ const Memes = () => {
           {filteredMeme.length > 0 ? (
             filteredMeme.map((meme, index) => (
               <div key={index} className="grid-item">
-                <img src={meme.url} alt={meme.name} />
+                <img src={meme.url} alt={meme.name} width={350} height={350} />
                 <div>{meme.name}</div>
                 <div>
                   {" "}
