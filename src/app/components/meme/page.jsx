755f3a994110b2fd5d16a77697b24c168bf37882
Ego Nwaekpe <glorynwaekpe@gmail.com>
2024-02-10 16:28:43 +0100

    adjusted the grid

--- src/app/components/meme/page.jsx
+++ src/app/components/meme/page.jsx
@@ -42,57 +42,6 @@ const Memes = () => {
   });
 
   return (
-    // <div className="bg-blue-500 min-h-screen text-white p-4">
-    //   <div className="max-w-4xl mx-auto">
-    //     <div className="mb-10 mt-10 flex justify-center">
-    //       <input
-    //         type="text"
-    //         placeholder="search meme..."
-    //         className="bg-blue-700 text-white px-4 py-2 rounded-l focus:outline-none"
-    //         onChange={(e) => setSearchMeme(e.target.value)}
-    //       />
-    //       <Link href="/">
-    //         <button className="bg-blue-700 px-4 py-2 rounded ml-[50px]">
-    //           Home
-    //         </button>
-    //       </Link>
-    //       <Link href="/components/upload">
-    //         <button className="bg-blue-700 px-4 py-2 rounded ml-[50px]">
-    //           Upload
-    //         </button>
-    //       </Link>
-    //     </div>
-
-    //     <div className="grid-container">
-    //       {filteredMeme.length > 0 ? (
-    //         filteredMeme.map((meme, index) => (
-    //           <div key={index} className="grid-item">
-    //             <img src={meme.url} alt={meme.name} width={350} height={350} />
-    //             <div>{meme.name}</div>
-    //             <div>
-    //               {" "}
-    //               <button
-    //                 onClick={() => handleDownload(meme.url, meme.name)}
-    //                 className="text-blue-700 font-bold"
-    //               >
-    //                 Download
-    //               </button>
-    //             </div>
-    //           </div>
-    //         ))
-    //       ) : (
-    //         <div className="error-message flex flex-col items-center justify-center text-center text-4xl font-bold text-red-500 p-8 bg-gray-200 rounded-md shadow-md ml-[350px]">
-    //           <span className="mb-4">Loading</span>
-    //           <div className="w-16 h-1 bg-red-500 rounded-full"></div>
-    //           <span className="mt-4 text-sm text-gray-600">
-    //             In few seconds....
-    //           </span>
-    //         </div>
-    //       )}
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="bg-blue-500 min-h-screen text-white p-4">
       <div className="max-w-4xl mx-auto">
         <div className="mb-10 mt-10 flex flex-col sm:flex-row justify-center">
