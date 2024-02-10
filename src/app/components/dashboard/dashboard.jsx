b202d508031d4f7f86ec61f0f456e16579dbfda6
Ego Nwaekpe <glorynwaekpe@gmail.com>
2024-02-10 15:06:14 +0100

    change the dashboard text

--- src/app/components/dashboard/dashboard.jsx
+++ src/app/components/dashboard/dashboard.jsx
@@ -4,7 +4,7 @@ import DashboardButton from "../dashboardButton/dashboard";
 const Dashboard = () => {
   return (
     <DashboardLayout>
-      <h1 className="text-2xl font-bold mb-4">AFCON Meme Hub Dashboard</h1>
+      <h1 className="text-2xl font-bold mb-4">Meme Hub </h1>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
         <DashboardButton text="View Memes" link="/components/meme" />
         <DashboardButton text="Upload Meme" link="/components/upload" />
